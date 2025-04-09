3b08ae5d0e84697f2e60131b0312b5ad116f10b0
Christof Becu <christof.becu@gmail.com>
2025-04-09 15:43:31 +0200

    feat(fish): update abbreviations for fish shell
    Add new abbreviation for 'cat' as 'ct' and remove unused 'ai' alias.
    
    - Added 'ct' abbreviation for 'cat' command.
    - Removed obsolete 'ai' abbreviation.
    - Improved clarity and usability of fish shell shortcuts.

--- .config/fish/conf.d/abbr_commands.fish
+++ .config/fish/conf.d/abbr_commands.fish
@@ -7,13 +7,12 @@ abbr -a diff 'colordiff'
 
 abbr -a dotfiles '/usr/bin/git --git-dir=/home/bedawang/.dotfiles/ --work-tree=/home/bedawang'
 abbr -a cat 'bat'
+abbr -a ct 'cat'
 
 abbr -a ls 'eza --icons'
 abbr -a lsa 'eza -lha -G --icons'
 
 abbr -a neofetch 'fastfetch'
-
-abbr -a ai 'ai.sh'
 
 alias pkgInfo="pacman -Qq | fzf --preview 'pacman -Qil {} | 
 bat -fpl yml' --layout=reverse --bind 'enter:execute(pacman -Qil 
