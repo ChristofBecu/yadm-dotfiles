6baf80a6554e1480ef9c344764c487a6029f30dc
Christof Becu <christof.becu@gmail.com>
2025-04-09 15:32:17 +0200

    feat(dotfiles): add background image to wezterm config
    Introduce a custom background image to the wezterm terminal
    configuration for enhanced aesthetics.
    
    - Set `window_background_image` to a local image path
    - Adjust background image brightness, hue, and saturation
    - Retain existing font and font size settings

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,8 +2,16 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+config.window_background_image = '/home/bedawang/gandalf-computer-wizzard.jpg'
+config.window_background_image_hsb = {
+  brightness = 0.1,
+  hue = 1.0,
+  saturation = 1.0,
+}
+
 config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 12.0
+
 
 return config
 
