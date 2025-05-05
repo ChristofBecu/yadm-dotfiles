efc8ac5ef282aad0cf7d591424a13694347deb81
Christof Becu <christof.becu@gmail.com>
2025-05-05 19:07:06 +0200

    feat(dotfiles): add Angular CLI to PATH for bash and fish
    Extend PATH configuration to include Angular CLI binary paths for
    both bash and fish shells, enabling easier CLI usage.
    
    - Updated .bash_path to include Angular CLI binary path.
    - Updated fish shell initialization to include Angular CLI binary path.

--- .config/fish/conf.d/01_init.fish
+++ .config/fish/conf.d/01_init.fish
@@ -5,6 +5,8 @@ set -x PATH $PATH /home/bedawang/bin/
 set -x PATH $PATH /home/bedawang/.dotnet
 set -x PATH $PATH /home/bedawang/.dotnet/tools
 set -x PATH $PATH /home/bedawang/.local/bin
+set -x PATH $PATH /home/bedawang/.local/share/nvm/v22.14.0/lib/node_modules/@angular/cli/bin
+
 zoxide init fish | source
 
 set fish_greeting
