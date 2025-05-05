e9e6e741d162f974fa45031ffac585c6ee6bd7e0
Christof Becu <christof.becu@gmail.com>
2025-05-05 14:01:01 +0200

    feat(dotfiles): update PATH with additional directories
    Add new directories to the PATH in Fish shell initialization to ensure
    access to tools and binaries.
    
    - Include .dotnet and .dotnet/tools for .NET tools.
    - Add .local/bin for user-specific executables.
    - Maintain compatibility with existing PATH entries.

--- .config/fish/conf.d/01_init.fish
+++ .config/fish/conf.d/01_init.fish
@@ -2,7 +2,9 @@ set -x PATH $PATH /home/bedawang/.cargo/bin
 set -x PATH $PATH /home/bedawang/scripts
 set -x PATH $PATH /home/bedawang/.local/share/nvm/v22.14.0/bin/
 set -x PATH $PATH /home/bedawang/bin/
-
+set -x PATH $PATH /home/bedawang/.dotnet
+set -x PATH $PATH /home/bedawang/.dotnet/tools
+set -x PATH $PATH /home/bedawang/.local/bin
 zoxide init fish | source
 
 set fish_greeting
