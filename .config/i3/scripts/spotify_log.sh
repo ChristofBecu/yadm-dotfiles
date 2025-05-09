ce1e5b86c5fbf173dcbd0b13a198fdc9642efc21
Christof Becu <christof.becu@gmail.com>
2025-05-09 11:23:34 +0200

    feat(i3): update Spotify integration scripts
    - Removed `spotify.sh` in favor of `spotify_log.sh` for better integration
    - Added support for `spotifycli` to log Spotify status to a file
    - Improved compatibility with Dunst notifications and i3status
    
    BREAKING CHANGE: `spotify.sh` has been removed; use `spotify_log.sh` instead.

--- .config/i3/scripts/spotify_log.sh
+++ .config/i3/scripts/spotify_log.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env bash
+
+# This script is triggered by dunst when a notification is received from Spotify.
+# https://github.com/dunst-project/dunst
+# In dunstrc, the following is added to the rules:
+#   [spotify]
+#   appname = Spotify
+#   urgency = normal
+#   script = ~/.config/i3/scripts/spotify_log.sh
+
+# In order to retrieve the metadata, spotifycli has to be installed.
+# https://github.com/pwittchen/spotify-cli-linux
+# --status (artist - song) is saved in a log file.
+
+# i3status is reading the log file to display the current status of Spotify in the i3status bar.
+
 /usr/bin/spotifycli --status > ~/logs/spotify.log
