ff615ea02b9056d14caef1c45055b0a1bddf2498
Christof Becu <christof.becu@gmail.com>
2025-07-01 17:08:18 +0200

    feat(dotfiles): add vcpkg path and root variables
    Add vcpkg path to the Fish shell PATH for easier access to binaries.
    Define VCPKG_ROOT environment variable for consistent usage.
    
    - Updated PATH in 01_init.fish to include vcpkg directory.
    - Added VCPKG_ROOT in 02_env_variables.fish for environment setup.

--- .config/fish/conf.d/01_init.fish
+++ .config/fish/conf.d/01_init.fish
@@ -6,6 +6,7 @@ set -x PATH $PATH /home/bedawang/.dotnet
 set -x PATH $PATH /home/bedawang/.dotnet/tools
 set -x PATH $PATH /home/bedawang/.local/bin
 set -x PATH $PATH /home/bedawang/.local/share/nvm/v22.14.0/lib/node_modules/@angular/cli/bin
+set -x PATH $PATH /home/bedawang/dev/external/vcpkg
 
 zoxide init fish | source
 
--- .config/fish/conf.d/02_env_variables.fish
+++ .config/fish/conf.d/02_env_variables.fish
@@ -1,3 +1,4 @@
 set -x GIT_CONFIG_GLOBAL $HOME/.config/git/config
 set -x EDITOR /usr/bin/nvim
+set -x VCPKG_ROOT /home/bedawang/dev/external/vcpkg
 source $HOME/.api_keys
