627353eb42a86dcf93a90b0208f9cedb453e46f8
Christof Becu <christof.becu@gmail.com>
2025-09-01 16:58:17 +0200

    feat(dotfiles): add DOOMWADDIR environment variable
    Add DOOMWADDIR environment variable to Fish shell configuration.
    
    - Set DOOMWADDIR to $HOME/doom for managing Doom WAD files.
    - Update Fish shell environment variables in 02_env_variables.fish.

--- .config/fish/conf.d/02_env_variables.fish
+++ .config/fish/conf.d/02_env_variables.fish
@@ -1,4 +1,5 @@
 set -x GIT_CONFIG_GLOBAL $HOME/.config/git/config
 set -x EDITOR /usr/bin/nvim
 set -x VCPKG_ROOT /home/bedawang/dev/external/vcpkg
+set -x DOOMWADDIR $HOME/doom
 source $HOME/.api_keys
