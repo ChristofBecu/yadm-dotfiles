365534d5bc418036b6eb45e29668ccff083c46d5
Christof Becu <christof.becu@gmail.com>
2025-10-06 22:30:54 +0200

    config(wezterm): enhance WezTerm font and appearance settings
    Improve font configuration and appearance in WezTerm for better
    readability and aesthetics.
    
    - Adjusted font settings to use a fallback list for better compatibility.
    - Increased font size and brightness for improved visibility.
    - Added options to refine glyph rendering and line height.
    
    BREAKING CHANGE: Font configuration now uses a fallback list, which may
    alter the appearance of text for some users.

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,14 +4,32 @@ local config = wezterm.config_builder()
 
 config.window_background_image = '/home/bedawang/gandalf-computer-wizzard.jpg'
 config.window_background_image_hsb = {
-  brightness = 0.1,
+  brightness = 0.2,
   hue = 1.0,
   saturation = 1.0,
 }
 
-config.font = wezterm.font("FiraCode Nerd Font")
-config.font_size = 12.0
+--config.font = wezterm.font("FiraCode Nerd Font")
+config.font = wezterm.font_with_fallback({
+  { family = "FiraCode Nerd Font" },
+  { family = "Symbols Nerd Font" },
+  { family = "DejaVu Sans Mono" },
+  { family = "JetBrains Mono" },
+  { family = "Noto Color Emoji", scale = 0.45 }
+})
+
+config.font_size = 13.0
+config.line_height = 1.0
+
+config.allow_square_glyphs_to_overflow_width = "Never" -- important for tables
+config.freetype_load_target = "Light" -- optional: can help with sharpness
+
+ --font = wezterm.font("Fira Mono"),
+  -- This forces ambiguous-width characters to be single-width
+  -- (see WezTerm docs for details)
+  -- allow_square_glyphs_to_overflow_width = false,
 
 
 return config
+
 
