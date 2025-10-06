36d85fc277cfab6c7486254d9a4ece3fe106b1a3
Christof Becu <christof.becu@gmail.com>
2025-10-06 22:19:38 +0200

    refactor(dotfiles): update spotify notification handling
    Refactored Spotify notification handling in dunst and i3 scripts to
    improve reliability and reduce dependency on Spotify's native behavior.
    
    - Commented out Spotify section in dunst config.
    - Updated i3 spotify_wrapper.sh to use a loop for periodic updates.
    - Removed reliance on Spotify process termination for status updates.

--- .config/i3/scripts/spotify_wrapper.sh
+++ .config/i3/scripts/spotify_wrapper.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 
 /usr/bin/spotify "$@" &
-SPOTIFY_PID=$!
 
-# Wait for Spotify to exit
-wait $SPOTIFY_PID
+# The notification of spotify to dunst is not longer reliable.
+# Therefore, we use a loop to update the status of spotify every 2 seconds.
 
-# Update status after Spotify closes
-~/.config/i3/scripts/spotify_log.sh
+# SPOTIFY_PID=$!
+
+# # Wait for Spotify to exit
+# wait $SPOTIFY_PID
+
+# # Update status after Spotify closes
+# ~/.config/i3/scripts/spotify_log.sh
+
+# Fork the process to run the logging script in the background
+(
+    # Initial run
+    ~/.config/i3/scripts/spotify_log.sh
+
+    # Then update every 2 seconds
+    while true; do
+        sleep 2
+        ~/.config/i3/scripts/spotify_log.sh
+    done
+) &
